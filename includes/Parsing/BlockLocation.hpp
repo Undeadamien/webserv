ae480719af0a9df113e5e775ece3fea18f64b65e
Enzo Viala <eviala@paul-f4Ar1s2.clusters.42paris.fr>
2025-01-16 13:20:04 +0100

    class pour le parsing

---
+++ includes/Parsing/BlockLocation.hpp
@@ -0,0 +1,37 @@
+#ifndef BLOCKLOCATION_HPP
+#define BLOCKLOCATION_HPP
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+# include <iomanip>
+
+# include "ConfigParser.hpp"
+
+enum e_Methods
+{
+	GET,
+	POST,
+	DELETE,
+	PUT,
+	UNKNOWN
+};
+
+enum e_boolMod
+{
+	FALSE,
+	TRUE
+};
+
+class BlocLocation
+{
+	private:
+	
+
+
+}
+
+
+
+#endif
