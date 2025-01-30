9265fa3498ce82fe8cd50da6f9f5916bc77139cb
undeadamien <damien.trala@outlook.com>
2025-01-30 18:41:50 +0100

    server little advancement

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -6,23 +6,33 @@
 /*   By: dtrala <dtrala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 23:44:36 by dtrala            #+#    #+#             */
-/*   Updated: 2025/01/24 11:44:03 by dtrala           ###   ########.fr       */
+/*   Updated: 2025/01/30 13:30:46 by dtrala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+#include <vector>
+
+#include "Socket.hpp"
+
 class Server
 {
 public:
-    Server();
-    Server(const Server &other);
-    Server &operator=(const Server &other);
-    ~Server();
+	Server();
+	Server(const Server &other);
+	Server &operator=(const Server &other);
+	~Server();
+
+	void start();
 
 protected:
 private:
+	std::vector<Socket> sockets;
+
+	void setup();
+	void run();
 };
 
 #endif
