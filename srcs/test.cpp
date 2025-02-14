9397681216ec6aeac4a05e98844070b9af948f30
undeadamien <damien.trala@outlook.com>
2025-02-14 14:57:23 +0100

    test rework, simple cgi and some minor modifications

--- srcs/test.cpp
+++ srcs/test.cpp
@@ -1,28 +1,61 @@
-#include "Arguments.hpp"
-#include "ConfParser.hpp"
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include <cerrno>
+#include <csignal>
+#include <cstdlib>
+#include <fstream>
+#include <map>
+
 #include "Request.hpp"
 #include "Response.hpp"
-#include "Server.hpp"
 #include "colors.hpp"
 #include "usefull.hpp"
 
+int fdSocket = -1;
+
+// could delete this signal handler and use a destructor
+void handle_signal(int signal)
+{
+	if (signal != SIGINT) return;
+	std::cout << "\rStopping the server " << std::endl;
+	if (fdSocket != -1) close(fdSocket);
+	exit(EXIT_SUCCESS);
+}
+
 std::string get_file_content(std::string path)
 {
-	(void)path;
 	std::ifstream inFile(path.c_str());
-	if (!inFile)
-		throw std::runtime_error("Error: opening '" + path + "'");
+	if (!inFile) throw std::runtime_error("Error: opening '" + path + "'");
 	std::string content;
 	std::string line;
 	while (std::getline(inFile, line)) content += line;
 	return (content);
 };
 
+std::string get_content_type(std::string target)
+{
+	std::string::size_type start = target.find_last_of(".") + 1;
+	if (start == std::string::npos) return "application/octet-stream";
+	std::string extension = target.substr(start);
+	if (extension == "css") return "text/css";
+	if (extension == "html") return "text/html";
+	if (extension == "jpeg") return "image/jpeg";
+	if (extension == "jpg") return "image/jpeg";
+	if (extension == "js") return "application/javascript";
+	if (extension == "json") return "application/json";
+	if (extension == "png") return "image/png";
+	return "application/octet-stream";
+}
+
 int simple_server()
 {
-	int fdSocket;
+	signal(SIGINT, handle_signal);
 	if ((fdSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		std::cerr << "Error: socket" << std::endl;
 		return (EXIT_FAILURE);
+	}
 	sockaddr_in address;
 	address.sin_family = AF_INET;
 	address.sin_port = htons(PORT);
@@ -31,11 +64,13 @@ int simple_server()
 	if (bind(fdSocket, (const sockaddr *)(&address), sizeof(address)) == -1)
 	{
 		close(fdSocket);
+		std::cerr << "Error: bind, error num " << errno << std::endl;
 		return (EXIT_FAILURE);
 	}
 	if (listen(fdSocket, 10) == -1)
 	{
 		close(fdSocket);
+		std::cerr << "Error: listen" << std::endl;
 		return (EXIT_FAILURE);
 	}
 
@@ -53,6 +88,7 @@ int simple_server()
 		if ((connection) == -1)
 		{
 			close(fdSocket);
+			std::cerr << "Error: accept" << std::endl;
 			return (EXIT_FAILURE);
 		}
 
@@ -78,32 +114,46 @@ int simple_server()
 		std::string contentLength;
 		std::string target = request.getTarget();
 		std::string root = "web";
-		if (target == "/")
-			target = "/index.html";
+		if (target == "/") target = "/index.html";
+
+		Response response;
 		try
 		{
 			content = get_file_content(root + target);
 			contentLength = itos(content.length());
+
+			std::map<std::string, std::string> headers;
+			headers["Content-Type"] = get_content_type(target);
+			headers["Content-Length"] = contentLength;
+
+			response.setProtocol("HTTP/1.1");
+			response.setStatusCode(200);
+			response.setStatusText("OK");
+			response.setHeaders(headers);
+			response.setBody(content);
 		}
 		catch (std::exception &e)
 		{
-			std::cerr << e.what() << std::endl;
+			std::cerr << e.what() << "\n";
+			content = "Ressource not found";
+
+			std::map<std::string, std::string> headers;
+			headers["Content-Type"] = "text/html";
+			headers["Content-Length"] = itos(content.length());
+
+			response.setProtocol("HTTP/1.1");
+			response.setStatusCode(404);
+			response.setStatusText("NOK");
+			response.setHeaders(headers);
+			response.setBody(content);
 		}
 
 		// need to change this to Response
-		std::string response;
-		response += "HTTP/1.1 200 OK\r\n";
-		if (target.find(".html") != std::string::npos)
-			response += "Content-Type: text/html\r\n";
-		else if (target.find(".css") != std::string::npos)
-			response += "Content-Type: text/css\r\n";
-		response += "Content-Length: " + contentLength + "\r\n";
-		response += "\r\n";
-		response += content;
+		std::string response_str = response.toString();
 		std::cout << UNDERLINE << "Response" << RESET << "\n"
-				  << response << "\n"
+				  << response.toString() << "\n"
 				  << std::endl;
-		send(connection, response.c_str(), response.size(), 0);
+		send(connection, response_str.c_str(), response_str.size(), 0);
 		close(connection);
 	}
 	close(fdSocket);
@@ -112,16 +162,18 @@ int simple_server()
 
 void testResponse()
 {
+	std::string content = "Body of test response";
+
 	std::map<std::string, std::string> headers;
-	headers["Content"] = "Hello";
-	headers["Language"] = "French";
+	headers["Content-Type"] = "text/html";
+	headers["Content-Length"] = itos(content.length());
 
 	Response response;
-	response.setProtocol("HTTP");
+	response.setProtocol("HTTP/1.1");
 	response.setStatusCode(200);
 	response.setStatusText("OK");
 	response.setHeaders(headers);
-	response.setBody("test");
+	response.setBody(content);
 
 	std::cout << response << std::endl;
 	std::cout << std::endl;
